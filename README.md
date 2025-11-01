7c67e7a8737a2de44624bc6d15224fe8fa6ec0a4
manelbc <84017890+manel-bc@users.noreply.github.com>
2025-11-01 16:31:41 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mute-ads
+
+Used to mute applications when ads are playing, for example, applications that sounds like "specify".
+
+## How to use
+
+1. Download [NirCmd](www.nirsoft.net) from their official site.
+2. Fill out the config.template.json and save it as `config.json` in the same folder.
+3. Run the script (you might have to change your system's scripts execution policies and/or unblock the file).
---
+++ mute-ads.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = "Stop"
+
+function Main {
+    while ($true) {
+        if (Is-App-Playing-Ads) {
+            Change-App-Volume 0
+            continue
+        }
+
+        Change-App-Volume 1
+        Start-Sleep -Milliseconds 500
+    }
+}
+
+function Is-App-Playing-Ads {
+    Get-Process $config.applicationName |
+        ForEach-Object { 
+            if ($_.MainWindowTitle -like "*Advertisement*") {
+                return $true
+            }
+        }
+       
+    return $false
+}
+
+function Change-App-Volume {
+    param (
+        [float]$volume
+    )
+
+    $executable = Get-Process $config.applicationName |
+        Select-Object -First 1 -ExpandProperty Path |
+        ForEach-Object { [System.IO.Path]::GetFileName($_) }
+
+    & $config.pathToNirCmd setappvolume $executable $volume
+}
+
+$configFile = Join-Path -Path $PSScriptRoot -ChildPath "config.json"
+$config = Get-Content $configFile -Raw | ConvertFrom-Json
+
+Main
